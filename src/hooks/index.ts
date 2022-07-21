55a5ebe6f7be2422582d358c34187fe7de9213a7
丘伟康 <642827353@qq.com>
2022-07-21 21:07:22 +0800

    fix: 移除多余代码

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 import usePrevious from './usePrevious'
-import useMount from './useMount'
 import useUnMount from './useUnMount'
 import useFirstMountState from './useFirstMountState'
 import useUpdateEffect from './useUpdateEffect'
 
-export { usePrevious, useMount, useUnMount, useFirstMountState, useUpdateEffect }
+export { usePrevious, useUnMount, useFirstMountState, useUpdateEffect }
--- src/hooks/useMount.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react'
-
-export default function useMount(fn: () => void) {
-  useEffect(() => {
-    fn?.()
-  }, [])
-}
