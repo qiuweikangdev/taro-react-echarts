4254068a7108084f2d92129344f44bc56f04bbf6
丘伟康 <642827353@qq.com>
2022-09-28 21:11:53 +0800

    chore: remove types

--- scripts/update-pkg.js
+++ scripts/update-pkg.js
@@ -7,14 +7,15 @@ if (process.env.NODE_ENV === 'prod') {
   Object.assign(pkg, {
     main: 'dist/index.js',
     module: 'dist/index.esm.js',
-    types: 'dist/index.d.ts',
   })
 } else {
   Object.assign(pkg, {
     main: 'src/index.ts',
     module: 'src/index.ts',
-    types: 'src/core/types.ts',
   })
 }
 
-writeFileSync(path.resolve(process.cwd(),'packages/echarts/package.json'), JSON.stringify(pkg, null, 2))
+writeFileSync(
+  path.resolve(process.cwd(), 'packages/echarts/package.json'),
+  JSON.stringify(pkg, null, 2),
+)
